5f36bb0be800dfa0ae4f4de454e0a2c8170d4cf7
Amirul Dev <73012169+amiruldev20@users.noreply.github.com>
2023-04-22 00:06:55 +0700

    Create index.js

---
+++ index.js
@@ -0,0 +1,40 @@
+/*
+© whatsapp-web.js
+re-developed by: Amirul Dev
+contact:
+- ig: @amirul.dev
+- github: amiruldev20
+- wa: 0851574894460
+*/
+
+'use strict';
+
+import Constants from './src/util/Constants';
+
+// Exporting all modules
+export { default as Client } from './src/Client';
+export { default as version } from './package.json';
+
+// Func
+export { default as Chat } from './src/func/Chat';
+export { default as PrivateChat } from './src/func/PrivateChat';
+export { default as GroupChat } from './src/func/GroupChat';
+export { default as Message } from './src/func/Message';
+export { default as MessageMedia } from './src/func/MessageMedia';
+export { default as Contact } from './src/func/Contact';
+export { default as PrivateContact } from './src/func/PrivateContact';
+export { default as BusinessContact } from './src/func/BusinessContact';
+export { default as ClientInfo } from './src/func/ClientInfo';
+export { default as Location } from './src/func/Location';
+export { default as ProductMetadata } from './src/func/ProductMetadata';
+export { default as List } from './src/func/List';
+export { default as Buttons } from './src/func/Buttons';
+
+// Auth 
+export { default as NoAuth } from './src/auth/NoAuth';
+export { default as LocalAuth } from './src/auth/LocalAuth';
+export { default as RemoteAuth } from './src/auth/RemoteAuth';
+export { default as LegacySessionAuth } from './src/auth/LegacySessionAuth';
+
+// Export constants as well
+export { Constants };
