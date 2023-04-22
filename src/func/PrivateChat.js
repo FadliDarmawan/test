3a80e095707c22229eb4bc1551f2b9f36724f973
Amirul Dev <73012169+amiruldev20@users.noreply.github.com>
2023-04-22 19:40:57 +0700

    Create PrivateChat.js

---
+++ src/func/PrivateChat.js
@@ -0,0 +1,13 @@
+'use strict';
+
+import Chat from './Chat'
+
+/**
+ * Represents a Private Chat on WhatsApp
+ * @extends {Chat}
+ */
+class PrivateChat extends Chat {
+
+}
+
+export default PrivateChat;
