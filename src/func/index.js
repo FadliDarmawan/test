2b6d45eed201cbe2fe70141f38d44f8508d86b1b
Amirul Dev <73012169+amiruldev20@users.noreply.github.com>
2023-04-22 19:48:10 +0700

    Create index.js

---
+++ src/func/index.js
@@ -0,0 +1,45 @@
+'use strict';
+
+import Base from './Base';
+import BusinessContact from './BusinessContact';
+import Chat from './Chat';
+import ClientInfo from './ClientInfo';
+import Contact from './Contact';
+import GroupChat from './GroupChat';
+import Location from './Location';
+import Message from './Message';
+import MessageMedia from './MessageMedia';
+import PrivateChat from './PrivateChat';
+import PrivateContact from './PrivateContact';
+import GroupNotification from './GroupNotification';
+import Label from './Label.js';
+import Order from './Order';
+import Product from './Product';
+import Call from './Call';
+import Buttons from './Buttons';
+import List from './List';
+import Payment from './Payment';
+import Reaction from './Reaction';
+
+export default {
+Base,
+BusinessContact,
+Chat,
+ClientInfo,
+Contact,
+GroupChat,
+Location,
+Message,
+MessageMedia,
+PrivateChat,
+PrivateContact,
+GroupNotification,
+Label,
+Order,
+Product,
+Call,
+Buttons,
+List,
+Payment,
+Reaction,
+};
