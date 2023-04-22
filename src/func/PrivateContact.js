b3c2a1e254b35d564b7120e58d364396b27b44c2
Amirul Dev <73012169+amiruldev20@users.noreply.github.com>
2023-04-22 19:42:11 +0700

    Create PrivateContact.js

---
+++ src/func/PrivateContact.js
@@ -0,0 +1,13 @@
+'use strict';
+
+import Contact from './Contact'
+
+/**
+ * Represents a Private Contact on WhatsApp
+ * @extends {Contact}
+ */
+class PrivateContact extends Contact {
+
+}
+
+export default PrivateContact;
